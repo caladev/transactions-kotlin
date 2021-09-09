ab26d180f07f3d580eb8184acb0dfffa74520d6b
Bao Tran <btrnaoa@gmail.com>
2021-09-10 09:33:30 +1000

    chore: update gradle run task

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,10 @@ dependencies {
     testImplementation(kotlin("test"))
 }
 
+tasks.getByName("run", JavaExec::class) {
+    standardInput = System.`in`
+}
+
 tasks.test {
     useJUnitPlatform()
 }

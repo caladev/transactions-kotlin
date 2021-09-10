74dc3bf76a607ca83606232d3bf81a5ca0a265fc
Bao Tran <btrnaoa@gmail.com>
2021-09-10 11:25:49 +1000

    :o

--- src/main/kotlin/utils/CsvReader.kt
+++
@@ -1,9 +0,0 @@
-package utils
-
-import java.io.File
-
-class CsvReader {
-    companion object {
-        fun rows(fileName: String) = File(fileName).bufferedReader().useLines { lines -> lines.drop(1).toList() }
-    }
-}
